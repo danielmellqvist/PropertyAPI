68c7394d9525a8f42314bde9892982648793e5ae
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-01 16:35:58 +0200

    Added some data annotations

--- Entities/Models/Comment.cs
+++ Entities/Models/Comment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,19 @@ namespace Entities.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Comment is a required field.")]
         [MaxLength(500, ErrorMessage ="Comments can be max 500 characters")]
         public string Content { get; set; }
+
         public DateTime CreatedOn { get; set; }
+
+        //[ForeignKey(nameof(User))]
         public Guid UserId { get; set; }
+
         public int RealEstateId { get; set; }
+
+
+        // Relational
     }
 }
