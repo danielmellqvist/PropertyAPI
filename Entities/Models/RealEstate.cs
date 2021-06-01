8e180a9461f867cec99d9f756b5c4e346d0a71a7
Louise Olsson <louise.m.k.olsson@hotmail.se>
2021-06-01 16:36:30 +0200

    Added models: -Address -ConstructionYear -RealEstateType -RealEstate (this one needs foreign keys)

---
+++ Entities/Models/RealEstate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class RealEstate
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        public uint? SellingPrice { get; set; }
+        public int? RentingPrice { get; set; }
+        public bool CanBeSold { get; set; }
+        public bool CanBeRented { get; set; }
+
+        //Relational
+        //[ForeignKey("")]
+        //public int AddressId { get; set; }
+        //public Address Address { get; set; }
+    }
+}
