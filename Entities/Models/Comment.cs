0e861eb357990e625781139a0e65e1fb3a1cd514
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-01 16:28:28 +0200

    Added comments model

--- Entities/Models/Comment.cs
+++ Entities/Models/Comment.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.Models
 {
-    class Comment
+    public class Comment
     {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(500, ErrorMessage ="Comments can be max 500 characters")]
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid UserId { get; set; }
+        public int RealEstateId { get; set; }
     }
 }
