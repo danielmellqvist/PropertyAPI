463c7ebb76dbd7335a81551094ef8f71dd60c466
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-01 16:45:16 +0200

    Added two classes

---
+++ Entities/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string UserName { get; set; }
+
+
+    }
+}
