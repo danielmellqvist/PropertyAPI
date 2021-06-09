81e37d83b56def1f79e24f3429161bf862b7560b
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-09 13:30:08 +0200

    Added mapping profile

--- Entities/DataTransferObjects/RealEstateDto.cs
+++ Entities/DataTransferObjects/RealEstateDto.cs
@@ -10,8 +10,8 @@ namespace Entities.DataTransferObjects
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int SellingPrice { get; set; }
-        public int RentingPrice { get; set; }
+        public uint? SellingPrice { get; set; }
+        public int? RentingPrice { get; set; }
         public bool CanBeSold { get; set; }
         public bool CanBeRented { get; set; }
     }
---
+++ WebAPI/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RealEstate, RealEstateDto>();
+        }
+    }
+}
