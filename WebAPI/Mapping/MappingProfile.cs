ee85932f225f33d38be985df6f39f862482f52cb
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-12 14:27:21 +0200

    Added mapping for RealEstatesDto

--- WebAPI/Mapping/MappingProfile.cs
+++ WebAPI/Mapping/MappingProfile.cs
@@ -29,11 +29,14 @@ namespace WebAPI.Mapping
 
             CreateMap<ConstructionYear, RealEstateDto>();
 
+            CreateMap<RealEstate, RealEstatesDto>();
+
             CreateMap<RealEstate, RealEstateDto>()
                 .IncludeMembers(source => source.ConstructionYear)
                 .ForMember(destination => destination.Address, y => y.MapFrom(source => $"{source.Street}, {source.ZipCode} {source.City}"))
                 .ForMember(destination => destination.CreatedOn, y => y.MapFrom(source => source.CreatedUtc.ToLocalTime()))
-                ;
+                .ForMember(destination => destination.ConstructionYear, y => y.MapFrom(source => source.ConstructionYear.Year))
+                .ReverseMap();
 
             CreateMap<CommentForCreationDto, Comment >();
 
