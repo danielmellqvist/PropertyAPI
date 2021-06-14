95f06b46f620f9a3f30415e9125f6d483e0d2b1e
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-14 16:27:38 +0200

    Configures Swagger to allow documentation

--- WebAPI/Extensions/ServiceExtensions.cs
+++ WebAPI/Extensions/ServiceExtensions.cs
@@ -5,8 +5,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Repository;
 using Repository.Contracts;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace WebAPI.Extensions
 {
@@ -44,6 +48,23 @@ namespace WebAPI.Extensions
         public static void ConfigureRepositoryManager(this IServiceCollection services)
         {
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "Real Estate API", 
+                    Version = "v1",
+                    Description = "An API that lets you handle real estate ads and comments."
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
             
     }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -97,11 +97,8 @@ namespace WebAPI
             services.AddControllers().AddJsonOptions(opt =>
                  opt.JsonSerializerOptions.PropertyNamingPolicy = null);
 
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-            });
+            services.ConfigureSwagger();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -116,6 +113,12 @@ namespace WebAPI
             else
             {
                 app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1");
+                    c.RoutePrefix = string.Empty;
+                    });
+
             }
 
             app.ConfigureExceptionHandler(logger);
