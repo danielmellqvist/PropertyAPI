4087be5c41e57f0c462838feb8cf6a0ed53b0fa9
Marcus Thenander <marcus.thenander13@gmail.com>
2021-06-15 18:24:37 +0200

    edit to comment

--- Repository/ContactRepository.cs
+++ Repository/ContactRepository.cs
@@ -25,6 +25,7 @@ namespace Repository
             foreach (var contact in contacts)
             {
                 var cleanNumber = Regex.Replace(contact.Telephone, @"[^0-9]+", "");
+                // this checks contains as it is possible that +46...... or vice versa is added in to the database and when cleaned will not take country code into account
                 if (cleanNumber.Contains(cleanTelephone) || cleanTelephone.Contains(cleanNumber))
                 {
                     return contact;
