4e4939f250db569d1897a051ab1e349f423a7e74
Daniel Mellqvist <daniel@ducktyping.se>
2021-06-17 11:02:07 +0200

    Restored the DTOs

--- Entities/DataTransferObjects/RealEstatePublicDto.cs
+++ Entities/DataTransferObjects/RealEstatePublicDto.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    public class RealEstatePublicDto : RealEstateInfoDto
+    public class RealEstatePublicDto
     {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int ConstructionYear { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public uint SellingPrice { get; set; }
+        public int RentingPrice { get; set; }
+        public bool CanBeSold { get; set; }
+        public bool CanBeRented { get; set; }
     }
 }
